b67de29250bd0e857f64cad93c586a25644ffefa
Nicholas <66987398+nicholasddd@users.noreply.github.com>
2025-03-30 16:22:40 +0700

    Update scene.js

--- scene.js
+++ scene.js
@@ -1,6 +1,5 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.150.1/examples/jsm/controls/OrbitControls.js';
-
+import * as THREE from 'https://unpkg.com/three@0.150.1/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.150.1/examples/jsm/controls/OrbitControls.js';
 
 let camera, scene, renderer, snowball;
 const clock = new THREE.Clock();
